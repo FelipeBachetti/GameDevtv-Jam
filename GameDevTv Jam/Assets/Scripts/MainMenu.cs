0903cf07a91aeddd93be75d10d2ac3c53842d306
basilisco09 <105323687+basilisco09@users.noreply.github.com>
2022-05-29 23:37:57 -0300

    Áudio do menu principal

--- GameDevTv Jam/Assets/Scripts/MainMenu.cs
+++ GameDevTv Jam/Assets/Scripts/MainMenu.cs
@@ -7,12 +7,20 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
+        FindObjectOfType<AudioManager>().Play("clickButton");
         SceneManager.LoadScene("Zombie");
         Time.timeScale = 1f;
     }
+    
+    public void Option()
+    {
+        FindObjectOfType<AudioManager>().Play("clickButton");
+    }
+    
     public void QuitGame()
     {
         Debug.Log("QUIT!");
         Application.Quit();
+        FindObjectOfType<AudioManager>().Play("clickButton");
     }
 }
